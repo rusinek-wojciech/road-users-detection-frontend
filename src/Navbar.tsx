802607ef2234c666d836b85598cd821887c039ab
iKinsure <wrotekpl@gmail.com>
2021-12-06 22:08:06 +0100

    main screen with material, colors mode

---
+++ src/Navbar.tsx
@@ -0,0 +1,53 @@
+import {
+  AppBar,
+  Box,
+  IconButton,
+  PaletteMode,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+
+interface Props {
+  mode: PaletteMode
+  toggleColorMode: () => void
+}
+
+export const Navbar = ({ mode, toggleColorMode }: Props) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar variant='dense'>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='open drawer'
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant='h6'
+            color='inherit'
+            component='div'
+            sx={{ flexGrow: 1 }}
+          >
+            Road Traffic Recognition
+          </Typography>
+          <IconButton
+            size='large'
+            aria-label='change color mode'
+            edge='end'
+            color='inherit'
+            onClick={toggleColorMode}
+          >
+            {mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
