e0b06212d7488a9c4eb8b892446ca9c4330cdf9d
iKinsure <wrotekpl@gmail.com>
2022-01-03 15:04:04 +0100

    config update

--- src/services/config.ts
+++ src/services/config.ts
@@ -19,115 +19,7 @@ const getModelPath = (model: string): string => {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const resnet50: Config = {
-  LABELS: [
-    {
-      name: 'bike',
-      color: 'red',
-    },
-    {
-      name: 'scooter',
-      color: 'yellow',
-    },
-  ],
-  TRESHOLD: 0.9,
-  MODEL_WIDTH: 640,
-  MODEL_HEIGHT: 640,
-  BOXES_INDEX: 2,
-  CLASSES_INDEX: 3,
-  SCORES_INDEX: 5,
-  PATH: getModelPath('road'),
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const kangaroo: Config = {
-  LABELS: [
-    {
-      name: 'kangaroo',
-      color: 'red',
-    },
-    {
-      name: 'other',
-      color: 'yellow',
-    },
-  ],
-  MODEL_WIDTH: 320,
-  MODEL_HEIGHT: 320,
-  TRESHOLD: 0.75,
-  BOXES_INDEX: 0,
-  CLASSES_INDEX: 5,
-  SCORES_INDEX: 4,
-  PATH: getModelPath('kangaroo-detector'),
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const resnet101: Config = {
-  LABELS: [
-    {
-      name: 'bike',
-      color: 'red',
-    },
-    {
-      name: 'scooter',
-      color: 'yellow',
-    },
-    {
-      name: 'rolls',
-      color: '#6495ED',
-    },
-    {
-      name: 'pedestrian',
-      color: '#7FFF00',
-    },
-    {
-      name: 'uto',
-      color: '#008B8B',
-    },
-  ],
-  TRESHOLD: 0.9,
-  MODEL_WIDTH: 640,
-  MODEL_HEIGHT: 640,
-  BOXES_INDEX: 2,
-  CLASSES_INDEX: 3,
-  SCORES_INDEX: 5,
-  PATH: getModelPath('resnet101'),
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ssdMobilenet: Config = {
-  LABELS: [
-    {
-      name: 'bike',
-      color: 'red',
-    },
-    {
-      name: 'scooter',
-      color: 'yellow',
-    },
-    {
-      name: 'rolls',
-      color: '#6495ED',
-    },
-    {
-      name: 'pedestrian',
-      color: '#7FFF00',
-    },
-    {
-      name: 'uto',
-      color: '#008B8B',
-    },
-  ],
-  MODEL_WIDTH: 640,
-  MODEL_HEIGHT: 640,
-  TRESHOLD: 0.75,
-  BOXES_INDEX: 5,
-  CLASSES_INDEX: 6,
-  SCORES_INDEX: 4,
-  PATH: getModelPath('ssd_mobilenet'),
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ssdMobilenet2: Config = {
   LABELS: [
     {
       name: 'bike',
@@ -156,8 +48,8 @@ const ssdMobilenet2: Config = {
   BOXES_INDEX: 5,
   CLASSES_INDEX: 3,
   SCORES_INDEX: 6,
-  PATH: getModelPath('ssd_mobilenet_2'),
+  PATH: getModelPath('ssd_mobilenet'),
 }
 
 // set current profle
-export const config: Config = ssdMobilenet2
+export const config: Config = ssdMobilenet
