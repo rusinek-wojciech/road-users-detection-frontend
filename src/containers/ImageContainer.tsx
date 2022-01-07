79d4988ea6ceb989d7ab76508da21e6fee843d0e
iKinsure <59565620+iKinsure@users.noreply.github.com>
2022-01-07 02:25:05 +0100

    working image fulscreen switching correctly

--- src/containers/ImageContainer.tsx
+++ src/containers/ImageContainer.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useLayoutEffect, useRef } from 'react'
-import { Card, CardActionArea, CardMedia } from '@mui/material'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { Card, CardActionArea, CardMedia, Dialog } from '@mui/material'
 import { GraphModel } from '@tensorflow/tfjs'
 import { DetectedObject, detect } from '../services/detection'
 import { draw } from '../services/drawing'
@@ -22,54 +22,83 @@ const ImageContainer = (props: Props) => {
     fullscreen = false,
   } = props
 
-  const cardRef = useRef<HTMLDivElement>(null)
+  const cardRef = useRef<HTMLDivElement | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const cardFullscreenRef = useRef<HTMLDivElement | null>(null)
+  const canvasFullscreenRef = useRef<HTMLCanvasElement | null>(null)
+
   const imageRef = useRef<HTMLImageElement | null>(null)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const fullscreenRef = useRef<boolean>(fullscreen)
+  const frameRequest = useRef<number>(0)
+
+  const [objects, setObjects] = useState<DetectedObject[]>([])
 
   useLayoutEffect(() => {
+    setObjects([])
     const img = new Image()
     img.src = imageSource
     imageRef.current = img
-  }, [imageSource])
-
-  useEffect(() => {
-    let frameRequest = 0
-    const canvas = canvasRef.current
-    const card = cardRef.current
-
-    if (imageRef?.current && canvas && card) {
-      const image = imageRef.current
-
-      const animateDraw = (objects: DetectedObject[]) => {
-        draw(
-          objects,
-          canvas,
-          card.clientWidth,
-          card.clientHeight,
-          image.naturalWidth,
-          image.naturalHeight
-        )
-        frameRequest = requestAnimationFrame(() => animateDraw(objects))
-      }
-
-      const animateDetect = async () => {
+    img.addEventListener(
+      'load',
+      async () => {
         const objects = await detect(
           model,
           modelConfig,
-          image,
-          image.naturalWidth,
-          image.naturalHeight
+          img,
+          img.naturalWidth,
+          img.naturalHeight
         )
-        cancelAnimationFrame(frameRequest)
-        animateDraw(objects)
-      }
+        setObjects(objects)
+      },
+      { once: true }
+    )
+  }, [imageSource, model, modelConfig])
 
-      image.addEventListener('load', () => animateDetect(), {
-        once: true,
-      })
+  useLayoutEffect(() => {
+    fullscreenRef.current = fullscreen
+  }, [fullscreen])
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const canvasFullscreen = canvasFullscreenRef.current
+    const card = cardRef.current
+    const cardFullscreen = cardFullscreenRef.current
+
+    if (
+      imageRef?.current &&
+      canvasFullscreen &&
+      cardFullscreen &&
+      canvas &&
+      card
+    ) {
+      const image = imageRef.current
+      const animateDraw = () => {
+        if (fullscreenRef.current) {
+          draw(
+            objects,
+            canvasFullscreen,
+            cardFullscreen.clientWidth,
+            cardFullscreen.clientHeight,
+            image.naturalWidth,
+            image.naturalHeight
+          )
+        } else {
+          draw(
+            objects,
+            canvas,
+            card.clientWidth,
+            card.clientHeight,
+            image.naturalWidth,
+            image.naturalHeight
+          )
+        }
+        frameRequest.current = requestAnimationFrame(animateDraw)
+      }
+      animateDraw()
     }
+
     return () => {
-      cancelAnimationFrame(frameRequest)
+      cancelAnimationFrame(frameRequest.current)
       if (canvas) {
         const ctx = canvas.getContext('2d')
         if (ctx) {
@@ -77,35 +106,85 @@ const ImageContainer = (props: Props) => {
         }
       }
     }
-  }, [model, modelConfig, imageSource])
+  }, [objects])
 
   return (
-    <Card ref={cardRef} style={{ width: 'fit-content', margin: 'auto' }}>
-      <CardActionArea onClick={onClickAction}>
-        <canvas
-          ref={canvasRef}
+    <>
+      <Dialog
+        keepMounted
+        open={fullscreen}
+        onClose={onClickAction}
+        fullScreen
+        style={{ width: '100%' }}
+      >
+        <Card
+          ref={cardFullscreenRef}
           style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            zIndex: 100,
-            width: '100%',
-            height: '100%',
+            width: 'fit-content',
+            marginLeft: 'auto',
+            marginRight: 'auto',
           }}
-        />
-        <CardMedia
-          style={{
-            width: '100%',
-            objectFit: 'contain',
-            maxHeight: fullscreen ? '100vh' : '83vh',
-          }}
-          component='img'
-          image={imageSource}
-          alt='detected image'
-        />
-      </CardActionArea>
-    </Card>
+        >
+          <CardActionArea onClick={onClickAction}>
+            <canvas
+              ref={canvasFullscreenRef}
+              style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                zIndex: 100,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+            <CardMedia
+              style={{
+                width: '100%',
+                objectFit: 'contain',
+                maxHeight: '100vh',
+              }}
+              component='img'
+              image={imageSource}
+              alt='detected image'
+            />
+          </CardActionArea>
+        </Card>
+      </Dialog>
+      <Card
+        ref={cardRef}
+        style={{
+          width: 'fit-content',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        <CardActionArea onClick={onClickAction}>
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              zIndex: 100,
+              width: '100%',
+              height: '100%',
+            }}
+          />
+          <CardMedia
+            style={{
+              width: '100%',
+              objectFit: 'contain',
+              maxHeight: '83vh',
+            }}
+            component='img'
+            image={imageSource}
+            alt='detected image'
+          />
+        </CardActionArea>
+      </Card>
+    </>
   )
 }
 
+// export default memo(ImageContainer)
 export default ImageContainer
