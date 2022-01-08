39bb720e593e62758f8958ddf82e166e16c56511
iKinsure <wrotekpl@gmail.com>
2022-01-08 04:23:35 +0100

    prod tf mode

--- src/hooks/useForceUpdate.ts
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from 'react'
-
-const useForceUpdate = () => {
-  const [state, updateState] = useState({})
-  const forceUpdate = useCallback(() => updateState({}), [])
-  return [state, forceUpdate] as const
-}
-
-export default useForceUpdate
